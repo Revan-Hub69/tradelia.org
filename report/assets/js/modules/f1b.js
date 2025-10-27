15651d5d991b1db6790ba5e583ac0e16797c0a1d
Revan-Hub69 <topofferte@protonmail.ch>
2025-10-27 21:53:04 +0100

    Update f1b.js

--- report/assets/js/modules/f1b.js
+++ report/assets/js/modules/f1b.js
@@ -251,7 +251,7 @@ function openF1DrawerPublic(data) {
       }
     ],
     blocking: false,
-    panelSize: "wide",
+    panelSize: "xl",
     footerButtons: mobileMode ? [] : footerButtonsDesktop,
     footerTabs: mobileMode ? tabDefs : []
   });
