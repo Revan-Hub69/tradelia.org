a103dd804d1614bac7f376988810cd2e87d72460
Revan-Hub69 <topofferte@protonmail.ch>
2025-10-28 00:00:04 +0100

    Update f1b.js

--- report/assets/js/modules/f1b.js
+++ report/assets/js/modules/f1b.js
@@ -252,7 +252,7 @@ function openF1DrawerPublic(data) {
       }
     ],
     blocking: false,
-    panelSize: "wide",
+    panelSize: "xl",
     footerButtons: mobileMode ? [] : footerButtonsDesktop,
     footerTabs: mobileMode ? tabDefs : []
   });
