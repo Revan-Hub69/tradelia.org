3ce95917b3a820147ad5be290f5a91a6c4b227b7
Revan-Hub69 <topofferte@protonmail.ch>
2025-11-09 20:32:46 +0100

    fix: creato file stub report-navigation.js per evitare errori di import

---
+++ report/assets/js/components/report-navigation.js
@@ -0,0 +1,17 @@
+// /report/assets/js/components/report-navigation.js
+// Componente Navigation per Report - Stub/Placeholder
+// Questo file è un placeholder per evitare errori di import
+
+import Logger from '../utils/logger.js';
+
+const reportNavigation = {
+  init: function(config = {}) {
+    Logger.debug('ReportNavigation', 'Navigation init chiamato (stub)', config);
+    // Stub: non fa nulla per ora
+    // TODO: Implementare navigazione report se necessario
+    return;
+  }
+};
+
+export { reportNavigation };
+
