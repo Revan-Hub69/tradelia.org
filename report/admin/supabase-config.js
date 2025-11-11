990338ec321ed81d14301995eefce43e91c263ef
Revan-Hub69 <topofferte@protonmail.ch>
2025-11-11 20:26:06 +0100

    Admin: aggiunge supabase-config pubblico

---
+++ report/admin/supabase-config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = "https://higkhlfjfhlecbtfnznx.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZ2tobGZqZmhsZWNidGZuem54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NTc5OTksImV4cCI6MjA3ODAzMzk5OX0.qlhVhGkfc0rU7-tUg9Fu40D67HQzHjZhkEdP4mAPqTw";
+export const REPORTS_BUCKET = "report-charts";
