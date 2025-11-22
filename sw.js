0a698373e16a452020f26a613e2c5585d67c40c9
Revan-Hub69 <topofferte@protonmail.ch>
2025-11-22 17:16:57 +0100

    chore: bump version to 2.1.0 - Dashboard: 154+ commit dedicati - Implementazioni complete: Excellence (92/100), Notifiche (8.0/10 compliance), PWA, Versionamento automatico

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
  */
 
 // Version number - increment this to force cache update
-const VERSION = "2.0.2";
+const VERSION = "2.1.0";
 const CACHE_NAME = `tradelia-ai-v${VERSION}`;
 const STATIC_CACHE = [
   "/",
