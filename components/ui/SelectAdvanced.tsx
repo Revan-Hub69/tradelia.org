bc4d4b5f9f39cd841d5ed078e2c8ce40bb4d2307
Revan-Hub69 <topofferte@protonmail.ch>
2025-12-01 13:09:27 +0100

    fix: Correct closing parentheses for map expression in SelectAdvanced

--- components/ui/SelectAdvanced.tsx
+++ components/ui/SelectAdvanced.tsx
@@ -262,7 +262,7 @@ export function SelectAdvanced({
                       </button>
                     ))}
                   </div>
-                ))}
+                ))
               </div>
             )}
           </div>
