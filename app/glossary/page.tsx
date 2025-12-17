cb1df2c91786ed2dd225e6743332e91d84193086
Revan-Hub69 <topofferte@protonmail.ch>
2025-12-17 20:01:38 +0100

    Delete app directory

--- app/glossary/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-// Glossario temporaneamente rimosso - troppo complesso
-export default function GlossaryPage() {
-  redirect('/dashboard/education');
-}
