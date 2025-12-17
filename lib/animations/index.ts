4f4fca34f02826d61da98942cb3640f317564ebc
Revan-Hub69 <topofferte@protonmail.ch>
2025-12-17 20:05:14 +0100

    Delete lib directory

--- lib/animations/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Animation Infrastructure
- * Centralized exports for animation system
- */
-
-export * from './variants';
-export * from './constants';
-export { useReducedMotion } from '../hooks/useReducedMotion';
--- lib/hooks/useIsAdmin.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useUserRole } from "./useUserRole";
-
-export function useIsAdmin(): boolean {
-  const { role } = useUserRole();
-  return role === "admin";
-}
