5050f4e389599282a80760125e41d6b5b4f08899
Revan-Hub69 <topofferte@protonmail.ch>
2025-12-18 01:15:26 +0100

    fix: disabilitato cache e service worker

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -12,7 +12,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="it">
-      <body>{children}</body>
+      <head>
+        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
+      </head>
+      <body>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            // Clear service worker and cache
+            if ('serviceWorker' in navigator) {
+              navigator.serviceWorker.getRegistrations().then(function(registrations) {
+                for(let registration of registrations) {
+                  registration.unregister();
+                }
+              });
+            }
+            if ('caches' in window) {
+              caches.keys().then(function(names) {
+                for (let name of names) {
+                  caches.delete(name);
+                }
+              });
+            }
+          `
+        }} />
+        {children}
+      </body>
     </html>
   )
 }
