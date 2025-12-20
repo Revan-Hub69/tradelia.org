f3f09fd3d6b0064317c816f0f33ea1c824ffaa33
Revan-Hub69 <topofferte@protonmail.ch>
2025-12-20 16:30:23 +0100

    fix: remove header from main layout to prevent duplication

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,5 @@
 import type { ReactNode } from 'react'
 
-import { SiteFooter } from '@/components/site/Footer'
-import { SiteHeader } from '@/components/site/Header'
-
 export default function MainLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <SiteHeader />
-      {children}
-      <SiteFooter />
-    </>
-  )
+  return children
 }
