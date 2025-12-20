1543fbb1f75628007642143b24be6cd7a43cefd8
Revan-Hub69 <topofferte@protonmail.ch>
2025-12-20 18:31:46 +0100

    fix: restore header and footer to main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,14 @@
 import type { ReactNode } from 'react'
 
+import { SiteFooter } from '@/components/site/Footer'
+import { SiteHeader } from '@/components/site/Header'
+
 export default function MainLayout({ children }: { children: ReactNode }) {
-  return children
+  return (
+    <>
+      <SiteHeader />
+      {children}
+      <SiteFooter />
+    </>
+  )
 }
